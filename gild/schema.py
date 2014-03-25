eeb4936b888ae183a10567811654ab303e1e69e5
jl237561 <mr.li.jinpeng@gmail.com>
2014-03-25 15:15:49 +0100

    ENH: add permission

--- gild/schema.py
+++ gild/schema.py
@@ -8,3 +8,56 @@
 ##########################################################################
 
 """cubicweb-gild schema"""
+
+from yams.buildobjs import EntityType
+from yams.buildobjs import RelationDefinition
+from yams.buildobjs import SubjectRelation
+from yams.buildobjs import String
+from yams.buildobjs import RichString
+from yams.buildobjs import Int
+from yams.buildobjs import Float
+from yams.buildobjs import Boolean
+from yams.buildobjs import Datetime
+
+from cubes.medicalexp.schema import Study
+
+
+##########################################################################
+# Cati schema modification
+##########################################################################
+class can_read(RelationDefinition):
+    subject = 'CWUser'
+    object = 'Study'
+    cardinality = '**'
+
+
+class can_modify(RelationDefinition):
+    subject = 'CWUser'
+    object = 'Study'
+    cardinality = '**'
+
+
+STUDY_PERMISSIONS = {
+    'read': ('managers', ERQLExpression('U can_read X')),
+    'add': ('managers', ERQLExpression('U can_modify X')),
+    'update': ('managers', ERQLExpression('U can_modify X')),
+    'delete': ('managers', ERQLExpression('U can_modify X')),
+}
+
+
+ENTITY_PERMISSIONS = {
+    'read': ('managers', ERQLExpression('X belong_to S, U can_read S')),
+    'add': ('managers', ERQLExpression('X belong_to S, U can_modify S')),
+    'update': ('managers', ERQLExpression('X belong_to S, U can_modify S')),
+    'delete': ('managers', ERQLExpression('X belong_to S, U can_modify S')),
+}
+
+
+RELATION_PERMISSIONS = {
+    'read': ('managers', 'users'),
+    'add': ('managers', RRQLExpression('S belong_to ST, U can_modify ST')),
+    'delete': ('managers', RRQLExpression('S belong_to ST, U can_modify ST'))
+}
+
+
+Study.__permissions__ = STUDY_PERMISSIONS
