5e89ac3656712120b7eabd7dcf09fb868509b285
jl237561 <mr.li.jinpeng@gmail.com>
2014-04-11 16:49:32 +0200

    ENH: put all schema modification in the gild cube

--- gild/schema.py
+++ gild/schema.py
@@ -7,4 +7,33 @@
 # for details.
 ##########################################################################
 
+from cubes.questionnaire.schema import QuestionnaireRun
+from cubes.neuroimaging.schema import Scan
+from cubes.genomics.schema import GenomicMeasure
+
+from yams.buildobjs import EntityType
+from yams.buildobjs import RelationDefinition
+from yams.buildobjs import SubjectRelation
+from yams.buildobjs import String
+from yams.buildobjs import RichString
+from yams.buildobjs import Int
+from yams.buildobjs import Float
+from yams.buildobjs import Boolean
+
+
 """cubicweb-gild schema"""
+QuestionnaireRun.remove_relation("concerns")
+QuestionnaireRun.add_relation(SubjectRelation(('Subject', 'SubjectGroup'),
+                                              cardinality='1*',
+                                              inlined=True),
+                                              name='concerns')
+
+Scan.remove_relation("concerns")
+Scan.add_relation(SubjectRelation(('Subject', 'SubjectGroup'),
+                                  cardinality='1*', inlined=True),
+                                  name='concerns')
+
+GenomicMeasure.remove_relation("concerns")
+GenomicMeasure.add_relation(SubjectRelation(('Subject', 'SubjectGroup'),
+                                            cardinality='1*', inlined=True),
+                                            name='concerns')
