19515df5fb75a0992bc003f154def576c32b8387
jl237561 <mr.li.jinpeng@gmail.com>
2014-04-14 09:33:15 +0200

    FIX: move all the modification into gild cube

--- gild/schema.py
+++ gild/schema.py
@@ -8,8 +8,19 @@
 ##########################################################################
 
 from cubes.questionnaire.schema import QuestionnaireRun
+from cubes.questionnaire.schema import Questionnaire
 from cubes.neuroimaging.schema import Scan
 from cubes.genomics.schema import GenomicMeasure
+from cubes.medicalexp.schema import Configuration
+from cubes.medicalexp.schema import Protocol
+from cubes.medicalexp.schema import Assessment
+from cubes.medicalexp.schema import GenericMeasure
+from cubes.medicalexp.schema import ProcessingRun
+from cubes.medicalexp.schema import GenericTest
+from cubes.medicalexp.schema import GenericTestRun
+from cubes.medicalexp.schema import inputs
+from cubes.medicalexp.schema import outputs
+
 
 from yams.buildobjs import EntityType
 from yams.buildobjs import RelationDefinition
@@ -22,18 +33,110 @@ from yams.buildobjs import Boolean
 
 
 """cubicweb-gild schema"""
+
+
+class FileEntry(EntityType):
+    """ Any resource file """
+    name = String(maxsize=256)
+    filepath = String(required=True, indexed=True, maxsize=256)
+    size = Int()
+    md5 = String(maxsize=32)
+
+
+class FileSet(EntityType):
+    """ A composite resource file set """
+    name = String(maxsize=256, required=True)
+    format = String(maxsize=256)
+    file_entries = SubjectRelation('FileEntry', cardinality='**')
+    related_study = SubjectRelation('Study', cardinality='1*', inlined=True, composite='object')
+    other_studies = SubjectRelation('Study', cardinality='**')
+
+
+class FileParameter(EntityType):
+    parameter = String(required=True, indexed=True)
+    file_set = SubjectRelation('FileSet', cardinality='1*', inlined=True)
+
+
 QuestionnaireRun.remove_relation("concerns")
 QuestionnaireRun.add_relation(SubjectRelation(('Subject', 'SubjectGroup'),
-                                              cardinality='1*',
-                                              inlined=True),
-                                              name='concerns')
+                              cardinality='1*',
+                              inlined=True),
+                              name='concerns')
 
 Scan.remove_relation("concerns")
 Scan.add_relation(SubjectRelation(('Subject', 'SubjectGroup'),
-                                  cardinality='1*', inlined=True),
-                                  name='concerns')
+                  cardinality='1*', inlined=True),
+                  name='concerns')
 
 GenomicMeasure.remove_relation("concerns")
 GenomicMeasure.add_relation(SubjectRelation(('Subject', 'SubjectGroup'),
-                                            cardinality='1*', inlined=True),
-                                            name='concerns')
+                            cardinality='1*', inlined=True),
+                            name='concerns')
+
+Configuration.remove_relation("external_resources")
+Configuration.add_relation(SubjectRelation('FileSet',
+                                           cardinality='**',
+                                           composite='subject'),
+                           name='external_resources')
+Protocol.remove_relation("external_resources")
+Protocol.add_relation(SubjectRelation('FileSet',
+                                      cardinality='**',
+                                      composite='subject'),
+                      name='external_resources')
+
+Assessment.remove_relation("external_resources")
+Assessment.add_relation(SubjectRelation('FileSet',
+                                        cardinality='**',
+                                        composite='subject'),
+                        name='external_resources')
+
+GenericMeasure.remove_relation("external_resources")
+GenericMeasure.add_relation(SubjectRelation('FileSet',
+                                            cardinality='**',
+                                            composite='subject'),
+                            name='external_resources')
+
+ProcessingRun.remove_relation("external_resources")
+ProcessingRun.add_relation(SubjectRelation('FileSet',
+                                           cardinality='**',
+                                           composite='subject'),
+                           name='external_resources')
+
+GenericTest.remove_relation("external_resources")
+GenericTest.add_relation(SubjectRelation('FileSet',
+                                         cardinality='**',
+                                         composite='subject'),
+                         name='external_resources')
+
+GenericTestRun.remove_relation("external_resources")
+GenericTestRun.add_relation(SubjectRelation('FileSet',
+                                            cardinality='**',
+                                            composite='subject'),
+                            name='external_resources')
+
+Questionnaire.remove_relation("external_resources")
+Questionnaire.add_relation(SubjectRelation('FileSet',
+                                           cardinality='*1',
+                                           composite='subject'),
+                              name='external_resources')
+
+QuestionnaireRun.remove_relation("external_resources")
+QuestionnaireRun.add_relation(SubjectRelation('FileSet',
+                                              cardinality='*1',
+                                              composite='subject'),
+                              name='external_resources')
+
+Scan.remove_relation("external_resources")
+Scan.add_relation(SubjectRelation('FileSet',
+                                  cardinality='*1',
+                                  composite='subject'),
+                  name='external_resources')
+
+GenomicMeasure.remove_relation("external_resources")
+GenomicMeasure.add_relation(SubjectRelation('FileSet',
+                                            cardinality='*1',
+                                            composite='subject'),
+                            name='external_resources')
+
+inputs.object = ('GenericTestRun', 'ScoreValue', 'FileParameter')
+outputs.object = ('GenericTestRun', 'ScoreValue', 'FileParameter')
